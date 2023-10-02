bd7eb295a09810e0d4e86e197d09939ee08cfc32
sathanxavier1998 <71935696+sathanxavier1998@users.noreply.github.com>
2023-10-03 01:56:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,11 +7,11 @@ API_HASH = environ.get("API_HASH", "3f6835286b03e000ab6d71b37cc35b92")
 BOT_TOKEN = environ.get("BOT_TOKEN", "5557012576:AAFzIKDUqFbtuaVF5O64ebSo1X4iNLZT97Y")
 SESSION = environ.get("SESSION", "BQHIlWYAHV6c2KPKuP8QlalgdncIcCYP9nFdhD8ZuIgGWPcEbSHpYlvu41PaWj2xaUljcvrusnzNthCxX0IcvMG0ZpynSwROD2cKybk0h99pcQdTCRarNYMPeYkUYcC_zaNRVdSS_WiTOV4ayeIU2RvNDKfhWeVYKF_1CQPAO40YE00otz_Z_aTPWZuMfWZhpvz8jn6Mj8oigb49sRwhTSzKo23cJ49CyirjnWi-sSo5NMg4vPtKBnbW5rrCi-JRf-Iuz3yIyMn1LIC1MCVlcDD5_A7JkTuh7HwCuzHL04SEh5DknVx4t9q_tmvZNck3ale1hQNUyQpy6zedsnsBjK_hbOYPYQAAAAFodbDbAA")
 TIME = int(environ.get("TIME", "3000"))
-GROUPS = [-1001570401050]
-for grp in environ.get("GROUPS").split():
+GROUPS = []
+for grp in environ.get("GROUPS", "-1001570401050").split():
     GROUPS.append(int(grp))
 ADMINS = [1745047302 6047510747]
-for usr in environ.get("ADMINS").split():
+for usr in environ.get("ADMINS", "1745047302" "6047510747").split():
     ADMINS.append(int(usr))
 
 START_MSG = "<b>Hai {},\nI'm a private bot of @cinema_villa_grp to delete group messages after a specific time</b>"
